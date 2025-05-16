609ac659d611955af07c94ad702a031b89ccddfb
mael <maelgarnier5@gmail.com>
2025-05-16 12:00:52 -0400

    popup plus simples

--- lib/dialogs/alert_dialog_builder.dart
+++ lib/dialogs/alert_dialog_builder.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:projet_picsou/widgets/ui/Text_field_widget.dart';
 
 class DialogBuilder {
   static Future<void> warning(BuildContext context, String title, String content) {
@@ -22,6 +23,32 @@ class DialogBuilder {
       },
     );
   }
+
+  /*
+  static Future<void> textInput(BuildContext context, String title, String labelText, Function(String) onSubmitted) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: TextFieldWidget(
+            labelText: ,
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(textStyle: Theme.of(context).textTheme.labelLarge),
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+   */
 
 
 }
